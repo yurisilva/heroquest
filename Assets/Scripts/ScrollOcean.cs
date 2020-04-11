f5b4ddee1c6352565479115d9ec7bda0ab36a1fd
layuri.silva <yuri.silva@thoughtworks.com>
2020-04-10 23:18:47 -0300

    Board, heroes and familiars

---
+++ Assets/Scripts/ScrollOcean.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollOcean : MonoBehaviour
+{
+    public float ScrollX = 0.02f;
+    public float ScrollY = 0.02f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetX = Time.time * ScrollX;
+        float offsetY = Time.time * ScrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
