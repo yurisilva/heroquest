161d92c7f7b8e33b5235714925d5dc67f7c5ea12
layuri.silva <yuri.silva@thoughtworks.com>
2020-04-12 10:19:14 -0300

    Dice roll script with dummy labels for score

---
+++ Assets/Scripts/RollDice.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RollDice : MonoBehaviour
+{
+   public GameObject dice;
+
+   public void GetResult()
+    {
+        dice.SetActive(true);
+        Text diceResultLabel = GameObject.Find("DiceResult").GetComponent<Text>();
+        diceResultLabel.text = new System.Random().Next(1, 7).ToString();
+
+    }
+}
