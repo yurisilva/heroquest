cef26e6c2f8a148fcdbdf55458f02610f921b302
layuri.silva <yuri.silva@thoughtworks.com>
2020-04-12 15:50:59 -0300

    Importing entities from script to Unity (1/?)

---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Table
+{
+    //public House[] HeroHouses;
+
+    //public Table()
+    //{
+    //    HeroHouses = new House[28];
+    //    for (int i = 1; i < 29; i++)
+    //    {
+    //        HeroHouses[i - 1] = new House(GameObject.Find("H" + i.ToString()).transform);
+    //    }
+    //}
+}
