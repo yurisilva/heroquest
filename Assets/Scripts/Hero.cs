af7ac609573956fa88b7b5acf72b5b09ee6b5312
layuri.silva <yuri.silva@thoughtworks.com>
2020-04-15 11:21:35 -0300

    Fixed first movement going one house too much

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
@@ -8,28 +7,18 @@ public class Hero : MonoBehaviour
     public float moveSpeed;
     public GameObject familiar;
 
-    private int housesToMove;
-    private bool willMove = false;
+    public int housesToMove;
+    public bool willMove = false;
     public House nextHouse;
-
-    public void Move(float moveSpeed, House houseToMoveTo)
-    {
-        this.moveSpeed = moveSpeed;
-        willMove = true;
-        nextHouse = houseToMoveTo;
-        nextHouse.SetPlayerOccupyingHouse(nextHouse.indexOneBased, gameObject);
-
-    }
 
     public void Move(float moveSpeed, int housesToMove)
     {
         this.housesToMove = housesToMove;
         this.moveSpeed = moveSpeed;
-        willMove = true;
         nextHouse = Table.GetHouse(gameObject.GetComponent<Hero>().houseIndex).nextHouse;
+        willMove = housesToMove > 0;
     }
 
-    //todo fix: first movement is 
     void Update()
     {
         if (willMove)
@@ -37,15 +26,15 @@ public class Hero : MonoBehaviour
             transform.LookAt(nextHouse.HeroPositionInThisHouse());
             transform.position = Vector3.MoveTowards(transform.position, nextHouse.GetComponent<House>().HeroPositionInThisHouse(), moveSpeed * Time.deltaTime);
 
-            if (Vector3.Distance(transform.position, nextHouse.HeroPositionInThisHouse()) == 0 && housesToMove > 0)
+            if (Vector3.Distance(transform.position, nextHouse.HeroPositionInThisHouse()) == 0 && housesToMove == 0)
+            {
+                willMove = false;
+            }
+            else if (Vector3.Distance(transform.position, nextHouse.HeroPositionInThisHouse()) == 0 && housesToMove > 0)
             {
                 housesToMove--;
                 nextHouse.SetPlayerOccupyingHouse(gameObject);
                 Move(moveSpeed, housesToMove);
-            }
-            else if (Vector3.Distance(transform.position, nextHouse.HeroPositionInThisHouse()) == 0 && housesToMove == 0)
-            {
-                willMove = false;
             }
 
         }
