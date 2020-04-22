1c34e090bfefac0bcc04865e48c4e866ebe1eb7c
layuri.silva <yuri.silva@thoughtworks.com>
2020-04-22 14:48:40 -0300

    New Scene with the Terrain for the actual game

---
+++ Assets/Scripts/MainCameraRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MainCameraRotation : MonoBehaviour
+{
+    public GameObject pivot;
+
+    void Update()
+    {
+        transform.LookAt(pivot.transform);
+        transform.Translate(Vector3.right * Time.deltaTime);
+
+    }
+}
